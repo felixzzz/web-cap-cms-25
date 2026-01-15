ea7410b69f34898a826ad3ec7297d9a57093f21e
rizalgamatecha <rizal@gamatecha.com>
2026-01-15 09:03:51 +0700

    fix: remove slug on banner group

--- database/migrations/2026_01_15_062853_add_table_banners.php
+++ database/migrations/2026_01_15_062853_add_table_banners.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -18,7 +17,6 @@ return new class extends Migration
         Schema::create('banner_groups', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('slug')->nullable();
             $table->json('banners')->nullable();
             $table->string('bulk_position')->nullable();
             $table->timestamps();
